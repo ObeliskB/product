448223cd3229574825868afbf5c558e16c4470f7
obeliskB <anusorn.b@gmail.com>
2023-09-26 10:59:59 +0700

    create app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container, Table } from "react-bootstrap";
 
 function App() {
+  const [product, setProduct] = useState([]);
+
+  const getData = () => {
+    fetch("http://localhost:8000/api/product/")
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.length > 0) {
+          setProduct(result);
+        } else {
+          setProduct(<></>);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h2 className="text-success">Product</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr className="table-primary text-center">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        { product.map((item) => (
+          <tr key={item.id}>
+            <td className="text-center">{item.id}</td>
+            <td>{item.name}</td>
+            <td className="text-center">{item.price}</td>
+          </tr>
+        ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
