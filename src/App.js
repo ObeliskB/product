7cb5ec3df276cb3cf64803ac70c8e33fbe49d4fb
obeliskB <anusorn.b@gmail.com>
2023-09-26 12:11:04 +0700

    create app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Container, Table } from "react-bootstrap";
+import { Container, Table, Button, Modal, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+function SaveData(jsonData) {
+  const url = "http://localhost:8000/api/product/";
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: jsonData,
+  }).catch((err) => console.log(err));
+}
 
 function App() {
   const [product, setProduct] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const onSubmit = (data) => {
+    let jsonData = JSON.stringify(data);
+    SaveData(jsonData);
+    handleClose();
+    window.location.reload();
+  };
 
   const getData = () => {
     fetch("http://localhost:8000/api/product/")
@@ -34,15 +56,49 @@ function App() {
           </tr>
         </thead>
         <tbody>
-        { product.map((item) => (
-          <tr key={item.id}>
-            <td className="text-center">{item.id}</td>
-            <td>{item.name}</td>
-            <td className="text-center">{item.price}</td>
-          </tr>
-        ))}
+          {product.map((item) => (
+            <tr key={item.id}>
+              <td className="text-center">{item.id}</td>
+              <td>{item.name}</td>
+              <td className="text-center">{item.price}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
+      <Button variant="primary" onClick={handleShow}>
+        Add Product
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter product name"
+                {...register("name")}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                step=".01"
+                {...register("price")}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
